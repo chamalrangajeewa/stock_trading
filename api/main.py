48be45ccc99b207e976ce576fe3594fe2f8e6f34
Chamal Rangajeewa <Chamal.Rangajeewa@gmail.com>
2025-03-28 17:15:31 +0000

    initial commit

---
+++ api/main.py
@@ -0,0 +1,72 @@
+import uvicorn
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
+from typing import List
+
+class Fruit(BaseModel):
+    name: str
+
+class Fruits(BaseModel):
+    fruits: List[Fruit]
+
+tags_metadata = [
+    {
+        "name": "users",
+        "description": "Operations with users. The **login** logic is also here.",
+    },
+    {
+        "name": "items",
+        "description": "Manage items. So _fancy_ they have their own docs.",
+        "externalDocs": {
+            "description": "Items external docs",
+            "url": "https://fastapi.tiangolo.com/",
+        },
+    },
+]
+
+
+app = FastAPI(
+    openapi_tags=tags_metadata,
+    title="ChimichangApp",
+    description="description",
+    summary="Deadpool's favorite app. Nuff said.",
+    version="0.0.1",
+    terms_of_service="http://example.com/terms/",
+    contact={
+        "name": "Deadpoolio the Amazing",
+        "url": "http://x-force.example.com/contact/",
+        "email": "dp@x-force.example.com",
+    },
+    license_info={
+        "name": "Apache 2.0",
+        "url": "https://www.apache.org/licenses/LICENSE-2.0.html",
+    },
+    debug=True)
+
+origins = [
+    "http://localhost:3000",
+    # Add more origins here
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+memory_db = {"fruits": []}
+
+@app.get("/fruits", response_model=Fruits, tags=["users"])
+def get_fruits():
+    return Fruits(fruits=memory_db["fruits"])
+
+@app.post("/fruits", response_model=Fruit, tags=["items"])
+def add_fruit(fruit: Fruit):
+    memory_db["fruits"].append(fruit)
+    return fruit
+    
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
